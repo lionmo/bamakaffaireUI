2e47be4c345ca4bd7d336359d917bb0894444c9b
Lionmo <49696974+lionmo@users.noreply.github.com>
2019-05-26 03:42:50 -0400

    Un peu sur le menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PageMissingComponent } from './page-missing/page-missing.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',   redirectTo: '', pathMatch: 'full' },
+  { path: '**', component: PageMissingComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageMissingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatToolbarModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
